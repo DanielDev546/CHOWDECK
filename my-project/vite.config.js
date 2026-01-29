303c79bdfb386745c04c85e02b1a61ed5b5dd262
Daniel <amunddaniel9@gmail.com>
2026-01-29 22:43:59 +0100

    Initial commit

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/CHOWDECK/",
   plugins: [
     tailwindcss(),
+    svelte(),
   ],
 })
 
+import { svelte } from '@sveltejs/vite-plugin-svelte'   
